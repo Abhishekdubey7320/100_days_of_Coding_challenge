f9ce73ab06133c5d602c658f445bd14c128f9500
ABHISHEK KUMAR DUBEY <114648865+Abhishekdubey7320@users.noreply.github.com>
2022-09-29 07:11:47 -0700

    Create sum.java

---
+++ day1/sum.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two any Number:");
+         int a= sc.nextInt();
+         int b = sc.nextInt();
+         int sum =a+b;
+        System.out.println("Sum of the Number: "+sum);
+    }
+}

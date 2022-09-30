852e7207f9c65d52d59cf15e4fa670cd86557c52
ABHISHEK KUMAR DUBEY <114648865+Abhishekdubey7320@users.noreply.github.com>
2022-09-30 07:45:02 -0700

    Create divisibleby.java

---
+++ days2/divisibleby.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class divsibleby {
+    public static void main(String[] args) {
+        for(int i=1;i<=1000;i++){
+            if(i%3==0 && i%7==0){
+                System.out.println("\t"+i);
+            }
+        }
+    }
+}

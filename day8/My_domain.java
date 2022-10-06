c5b863d17ab4f543f9e13d9de5b41ad776162985
ABHISHEK KUMAR DUBEY <114648865+Abhishekdubey7320@users.noreply.github.com>
2022-10-06 23:50:34 +0530

    Create My_domain.java

---
+++ day8/My_domain.java
@@ -0,0 +1,24 @@
+What is HTML ?
+  
+HTML stands for Hyper Text Markup Language which is used for creating web pages and web applications.
+Hyper Text: HyperText simply means "Text within Text."
+Markup language: A markup language is a computer language that is used to apply layout and formatting conventions to a text document. 
+Markup language makes text more interactive and dynamic. It can turn text into images, tables, links, etc.
+Web Page: A web page is a document which is commonly written in HTML and translated by a web browser.
+
+Let's see a simple example of HTML.
+
+<!DOCTYPE>  
+<html>  
+<head>  
+<title> page title</title>  
+</head>  
+<body>  
+<h1>My 1st Day in my domain</h1>  
+<p>My 1st Day in my domain</p>  
+</body>  
+</html>
+
+
+
+# We Explain this example in my 2nd day of my domain.

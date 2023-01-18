a488b8422d8055ea5439e68bda873681f1e94612
ABHISHEK KUMAR DUBEY <114648865+Abhishekdubey7320@users.noreply.github.com>
2023-01-18 22:17:43 +0530

    Create Insert_Position.java

---
+++ day89/Insert_Position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+
+    while (lo <= hi) {
+        int mid =  (hi + lo) / 2;
+
+        if (target < nums[mid]) hi = mid - 1;
+        else if (target > nums[mid]) lo = mid + 1;
+        else return mid;
+    }
+    return lo;
+    }
+}
